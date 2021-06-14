609ffa6b77761127295b87a3050d21f52b92ba2a
Vineet Kalghatgi <vkalghat@gmail.com>
2021-06-15 03:43:31 +0530

    removed log statements

--- app.py
+++ app.py
@@ -47,31 +47,13 @@ def preprocess_context(raw_context, tokenized_question_length):
     raw_context_sentences = sent_tokenize(raw_context)
 
     return sentences_to_chunks(raw_context_sentences, chunk_length)
-    
-    # #Removing special [SEP] and [CLS] tokens at 0th and n-1th indices, as they need to be added seperaely for each chunk.
-    # stripped_context_tokens = tokenized_context.ids[1:tokens_length - 1]
-    # stripped_context_offsets = tokenized_context.offsets[1:tokens_length - 1]
 
-    # context_id_chunks = [stripped_context_tokens[i:i + chunk_length]
-    #                      for i in range(0, len(stripped_context_tokens), chunk_length)]
-    # context_offset_chunks = [stripped_context_offsets[i:i + chunk_length]
-    #                          for i in range(0, len(stripped_context_offsets), chunk_length)]
-
-    # for context_id_chunk, context_offset_chunk in zip(context_id_chunks, context_offset_chunks):
-        
-    #     #Add [SEP] and [CLS] tokens and their respective offsets which have a default value of (0,0)
-    #     context_id_chunk = [101] + context_id_chunk + [102]
-    #     context_offset_chunk = [(0, 0)] + context_offset_chunk + [(0,0)]
-
-
-    #return context_id_chunks, context_offset_chunks
 
 def predict_answer(raw_question, raw_context):
     #Takes in a raw question and raw context and returns an answer in raw string format.
 
     tokenized_question = tokenizer.encode(raw_question)
     tokenized_context_chunks, context_offset_chunks, raw_sentence_chunks = preprocess_context(raw_context, len(tokenized_question))
-    print(type(tokenized_context_chunks))
     x = create_input_targets(tokenized_question, tokenized_context_chunks)
 
     pred_start, pred_end = model.predict(x).values()
@@ -79,7 +61,6 @@ def predict_answer(raw_question, raw_context):
     confidence = -1
 
     for chunk_idx, (context_offset_chunk, raw_sentence_chunk) in enumerate(zip(context_offset_chunks, raw_sentence_chunks)):
-        print('Searching in chunk', chunk_idx+1, "...", len(context_offset_chunk))
         for idx, (start, end) in enumerate(zip(pred_start, pred_end)):
             offsets = context_offset_chunk
             chunk_confidence = max(start) + max(end)
@@ -96,7 +77,6 @@ def predict_answer(raw_question, raw_context):
                 pred_ans = raw_sentence_chunk[pred_char_start:offsets[end][1]]
             else:
                 pred_ans = raw_sentence_chunk[pred_char_start:]
-            print('answer : ', pred_ans, chunk_confidence)
             if chunk_confidence > confidence:
                     confidence = chunk_confidence
                     final_answer = pred_ans
@@ -120,7 +100,6 @@ def sentences_to_chunks(sentences,chunk_length):
         sent_offsets = sent.offsets[1:len(sent.offsets) - 1]
         
         if size + len(sent_ids) > chunk_length:
-            print('Chunk lengths : ', len(chunk_lookup['curr_chunk_offsets']), len(chunk_lookup['curr_chunk']))
             chunk_lookup['curr_chunk'] = tokenizer.encode(raw_chunk).ids
             chunk_lookup['curr_chunk_offsets'] = tokenizer.encode(raw_chunk).offsets
             padding_length = chunk_length - size
@@ -142,7 +121,6 @@ def sentences_to_chunks(sentences,chunk_length):
         raw_chunk += raw_sent 
 
     padding_length = chunk_length - size
-    print('Chunk lengths outside loop: ', len(chunk_lookup['curr_chunk_offsets']), len(chunk_lookup['curr_chunk']))
     chunk_lookup['curr_chunk'] = tokenizer.encode(raw_chunk).ids
     chunk_lookup['curr_chunk_offsets'] = tokenizer.encode(raw_chunk).offsets
     if padding_length > 0:
